ed22d5b61b7888e5b61a1899d3a612099689eca0
PurviMec <princy.mecwan94@gmail.com>
2022-01-17 19:41:07 -0500

    commit

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,14 +1,14 @@
 const commentFormHandler = async function(e) {
     e.preventDefault();
 
-    const postId = document.querySelector('input[name="post-id"]').value;
+    const blogId = document.querySelector('input[name="post-id"]').value;
     const body = document.querySelector('textarea[name="comment-body"]').value;
     
     if(body) {
         await fetch ('/api/comment', {
             method: 'POST',
             body: JSON.stringify({
-                postId,
+                blogId,
                 body
             }),
             headers: {
@@ -19,5 +19,6 @@ const commentFormHandler = async function(e) {
     }
 };
 
-document.querySelector('#new-comment-form');
-document.addEventListener('submit', commentFormHandler);
+document
+.querySelector('#new-comment-form')
+.addEventListener('submit', commentFormHandler);
--- server.js
+++ server.js
@@ -39,8 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(' NOW LISTENING ')
-    })
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
