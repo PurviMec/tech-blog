039fbd440b60e57e59e332ab5162348e69b45ef2
PurviMec <princy.mecwan94@gmail.com>
2022-01-18 22:28:19 -0500

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,8 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -9,3 +14,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+
+module.exports = sequelize;
